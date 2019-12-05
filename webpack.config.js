3551f239f99f0278570ab20d492386ef47142c01
Derek Barrera <derek@ethos.io>
2019-12-05 15:11:23 -0700

    setup the build system as all ou need to do is change the config.tsx file over from dev to prod add universal config object

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,12 @@ module.exports = async function(env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
   // Customize the config before returning it.
 
-  config.plugins[config.plugins.length - 1].config.navigateFallbackBlacklist = [
-    ...config.plugins[config.plugins.length - 1].config.navigateFallbackBlacklist,
-    /login|consent|admin|forgot_password/, // oauth pages
-  ]
+  if (Boolean(process.env.BUILD) == true) {
+    config.plugins[config.plugins.length - 1].config.navigateFallbackBlacklist = [
+      ...config.plugins[config.plugins.length - 1].config.navigateFallbackBlacklist,
+      /login|consent|admin|forgot_password/, // oauth pages
+    ]
+  }
 
   return config;
 };
